7a4057dd56d88be3d98e1adbc5d520ad10d94a24
Marcelina Kucięba <mkucieba@egnyte.com>
2019-07-03 19:45:10 +0200

    Add scripts for prod and dev modes

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,34 @@
 const path = require("path");
 
-module.exports = {
-    entry: "./src/index.js",
-    output: {
-        path: path.resolve(__dirname, "build"),
-        filename: "app.bundle.js"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: "babel-loader"
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    { loader: "style-loader" },
-                    {
-                        loader: "css-loader",
-                        options: {
-                            modules: {
-                                localIdentName: "[local]--[hash:base64:5]"
+module.exports = env => {
+    return {
+        mode: env || "production",
+        entry: "./src/index.js",
+        output: {
+            path: path.resolve(__dirname, "build"),
+            filename: "app.bundle.js"
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    loader: "babel-loader"
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        { loader: "style-loader" },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                modules: {
+                                    localIdentName: "[local]--[hash:base64:5]"
+                                }
                             }
                         }
-                    }
-                ]
-            }
-        ]
-    }
+                    ]
+                }
+            ]
+        }
+    };
 };
